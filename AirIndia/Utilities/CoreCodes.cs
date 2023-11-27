6a35a64988f9572c4d226c496b11e6efaba7f90a
182278 <182278@G848LTRV>
2023-11-27 19:21:20 +0530

    Add project files.

---
+++ AirIndia/Utilities/CoreCodes.cs
@@ -0,0 +1,107 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Serilog;
+
+namespace BunnyCart.Utilities
+{
+    internal class CoreCodes
+    {
+        Dictionary<string, string>? properties;
+        public IWebDriver driver;
+
+        public ExtentReports extent;
+        ExtentSparkReporter sparkReporter;
+        public ExtentTest test;
+        public void ReadConfigSettings()
+        {
+            string currDir = Directory.GetParent(@"../../../").FullName; //getting the working directory
+            properties = new Dictionary<string, string>();               //declaring the dictionary
+            string filename = currDir + "/ConfigSetting/config.properties"; //taking the file from working directory
+            string[] lines = File.ReadAllLines(filename);
+
+            foreach (string line in lines)                               //for geting the file data even if there are whitespaces
+            {
+                if (!string.IsNullOrWhiteSpace(line) && line.Contains("="))
+                {
+                    string[] parts = line.Split('=');
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+                    properties[key] = value;
+                }
+            }
+        }
+        [OneTimeSetUp]
+        public void InitializeBrowser()
+        {
+            string currDir = Directory.GetParent(@"../../../").FullName;
+            extent = new ExtentReports();
+            sparkReporter = new ExtentSparkReporter(currDir + "/ExtentReports/extent-report"
+                + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".html");
+
+            extent.AttachReporter(sparkReporter);
+            ReadConfigSettings();
+            if (properties["browser"].ToLower() == "chrome")
+            {
+                driver = new ChromeDriver();
+            }
+            else if (properties["browser"].ToLower() == "edge")
+            {
+                driver = new EdgeDriver();
+            }
+            driver.Url = properties["baseUrl"];
+            driver.Manage().Window.Maximize();
+        }
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+            extent.Flush();
+
+        }
+        public bool CheckLinkStatus(string url)
+        {
+            try
+            {
+                var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+                request.Method = "HEAD";
+                using (var response = request.GetResponse())
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static void ScrollIntoView(IWebDriver driver, IWebElement element)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", element);
+        }
+
+        public void LogTestResult(string testName, string result, string errorMessage = null)
+        {
+            Log.Information(result);
+            test = extent.CreateTest(testName);
+            if(errorMessage == null)
+            {
+                Log.Information(testName + "passed");
+                test.Pass(result);
+            }
+            else
+            {
+                Log.Error($"Test failed for {testName} \n Exception: \n{errorMessage}");
+                test.Fail(result);
+            }
+        }
+    }
+}
